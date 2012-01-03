3fabcc2bd89ca841d0f0c05b1b9bd32896f5c352
Cameron Seebach <cameron.seebach@gmail.com>
2012-01-02 20:38:48 -0800

    transition to states complete + more battle music

---
+++ states/battle.lua
@@ -0,0 +1,179 @@
+require "math"
+require "table"
+
+require "libs.middleclass"
+require "libs.art"
+local Soundtrack = require "libs.soundtrack"
+
+local Car = require "objects.car"
+local Monster = require "objects.monster"
+local Net = require "objects.net"
+local Ball = require "objects.ball"
+local Fans = require "objects.fans"
+
+local Battle = class("Battle")
+
+local function vectorLength(x, y)
+    return math.sqrt(x*x + y*y)
+end
+
+function Battle:startCrashParticle(position, impact)
+    local system = self.particle_systems[self.particle_systems.next]
+    self.particle_systems.next = ((self.particle_systems.next + 1)%10) + 1
+    if system then
+        system:setPosition(position[1], position[2])
+        system:setDirection(math.atan2(impact[2], impact[1]))
+        local impact_force = vectorLength(impact[2], impact[1])
+        system:setEmissionRate(impact_force*80)
+        if not system:isActive() then
+            system:start()
+        end
+    end
+end
+
+local function makeCollide(state)
+    local function collide(a, b, collision)
+        local objects = {}
+        if a then
+            objects[a] = true
+        end
+        if b then
+            objects[b] = true
+        end
+        if objects["red net"] and objects["ball"] then
+            state.fans:goalScored()
+            state.blue_score = state.blue_score + 1
+            state.ball:reset()
+        end
+        if objects["blue net"] and objects["ball"] then
+            state.fans:goalScored()
+            state.red_score = state.red_score + 1
+            state.ball:reset()
+        end
+        if objects["blue car"] and objects["red car"] then
+            if state.crash3:isStopped() then
+                state.crash3:play()
+            end
+            state:startCrashParticle({collision:getPosition()},
+                                     {collision:getVelocity()})
+        end
+        if objects["boundary"] and (objects["blue car"] or objects["red car"]) then
+            state:playCrash()
+            state:startCrashParticle({collision:getPosition()},
+                                     {collision:getVelocity()})
+        end
+    end
+    return collide
+end
+
+function Battle:playCrash()
+    rnum = math.random(3)
+    if rnum == 1 then
+        self.crash2:stop()
+        self.crash1:play()
+    end
+    if rnum == 2 then
+        self.crash1:stop()
+        self.crash2:play()
+    end
+end
+
+function Battle:load()
+    love.graphics.setColor(255, 255, 255)
+    love.graphics.setBackgroundColor(34, 189, 40)
+
+    self.red_score = 0
+    self.blue_score = 0
+
+    local world = love.physics.newWorld(0, 0, 800, 600)
+    world:setCallbacks(makeCollide(self), nil, nil, nil)
+    self.world = world
+
+    bound_bottom_body = love.physics.newBody(world, 400, 600)
+    bound_bottom_shape = love.physics.newRectangleShape(bound_bottom_body, 0, 0, 800, 3)
+    bound_bottom_shape:setData("boundary")
+    bound_left_body = love.physics.newBody(world, 0, 350)
+    bound_left_shape = love.physics.newRectangleShape(bound_left_body, 0, 0, 3, 600)
+    bound_left_shape:setData("boundary")
+    bound_right_body = love.physics.newBody(world, 800, 350)
+    bound_right_shape = love.physics.newRectangleShape(bound_right_body, 0, 0, 3, 600)
+    bound_right_shape:setData("boundary")
+
+    local objects = {Fans(world), Monster(1, 2, world), Monster(2, 2, world), Ball(world),
+                     Net(1, world), Net(2, world)}
+    self.fans = objects[1]
+    self.blue_car = objects[2]
+    self.red_car = objects[3]
+    self.ball = objects[4]
+    self.objects = objects
+
+    self.soundtrack = Soundtrack{"dope.mod", "iphar_aldeas_funk.mod"}
+    self.soundtrack:play()
+
+    --art("game start.ogg"):play()
+
+    love.graphics.setFont(love.graphics.newFont(20))
+
+    self.crash1 = art("crash-01.ogg")
+    self.crash2 = art("crash-02.ogg")
+    self.crash3 = art("crash-03.ogg")
+
+    self.spark = art("spark.png")
+    self.particle_systems = {next=1}
+    for i = 1, 10 do
+        local system = love.graphics.newParticleSystem(self.spark, 20)
+        system:setLifetime(.2)
+        system:setEmissionRate(120)
+        system:setParticleLife(1)
+        system:setSpread(2.5)
+        system:setSpeed(10,120)
+        system:setRadialAcceleration(10)
+        system:setTangentialAcceleration(0)
+        system:setSize(1)
+        system:setSizeVariation(0.5)
+        system:setRotation(0)
+        system:setSpin(0)
+        system:setSpinVariation(0)
+        system:setColor(255,255,255,240,255,255,255,10)
+        system:stop()
+        table.insert(self.particle_systems, system)
+    end
+end
+
+function Battle:draw()
+    love.graphics.clear()
+
+    --draw the game objects
+    for i, object in ipairs(self.objects) do
+        object:draw()
+    end
+
+    -- draw particles
+    for i, system in ipairs(self.particle_systems) do
+        love.graphics.draw(system)
+    end
+
+    --draw the score
+    love.graphics.setColor(255, 255, 255)
+
+    love.graphics.print(tostring(self.red_score), 60, 350)
+    love.graphics.print(tostring(self.blue_score), 710, 350)
+end
+
+function Battle:update()
+    self.world:update(1/60)
+
+    for i, system in ipairs(self.particle_systems) do
+        system:update(1/60)
+    end
+
+    for i, object in ipairs(self.objects) do
+        object:update()
+    end
+
+    return self
+end
+
+function Battle:keypressed(key) end
+
+return Battle
---
+++ states/mainmenu.lua
@@ -0,0 +1,110 @@
+require "libs.art"
+local Soundtrack = require "libs.soundtrack"
+
+local Battle = require "states.battle"
+
+local state = {}
+
+local function startGame(num_players)
+	state.fadingOut = true
+	state.fadeoutOpacity = 0
+	state.music:fadeOut(30)
+	state.num_players = num_players
+end
+
+function state:load()
+	love.graphics.setBackgroundColor(0, 0, 0)
+	self.title = art("title.png")
+	self.music = Soundtrack{"title.ogg", "w_a_r_by_awesome.xm",
+							"aceman_-_my_first_console.xm"}
+
+	self.selection = 1
+	self.choices = {{"2-Player", startGame, {2}},
+					--{"4-Player", startGame, {self, 4}},
+					{"Setup", goToSetup, nil}}
+
+	self.fadeInLength = 90
+	self.counter = 0
+
+	self.title_y = 44
+	self.selection_opacity = 0
+
+	self.selection_min_y = 250
+	self.selection_max_y = 500
+
+	love.graphics.setFont(love.graphics.newFont(30))
+
+	self.music:play()
+	return true
+end
+
+function state:keypressed(key)
+	if key == "up" then
+		self.selection = self.selection - 1
+	elseif key == "down" then
+		self.selection = self.selection + 1
+	end
+	if self.selection > #self.choices then
+		self.selection = 1
+	elseif self.selection < 1 then
+		self.selection = #self.choices
+	end
+
+	if key == "return" or key == " " then
+		self.choices[self.selection][2]()
+	end
+end
+
+function state:update()
+	self.music:update()
+	self.counter = self.counter + 1
+
+	if self.counter > 260 and self.counter < 280 then
+		self.title_y = self.title_y - 8
+	end
+
+	if self.counter > 290 and self.counter < 310 then
+		self.selection_opacity = self.selection_opacity + 16
+		self.selection_opacity = math.min(255, self.selection_opacity)
+	end
+
+	if self.fadingOut then
+		self.fadeoutOpacity = self.fadeoutOpacity + 10
+		self.fadeoutOpacity = math.min(255, self.fadeoutOpacity)
+		if self.fadeoutOpacity >= 255 then
+			local battle_state = Battle()
+			battle_state:load()
+			return battle_state
+		end
+	end
+
+	return self
+end
+
+function state:draw()
+	love.graphics.clear()
+
+	--draw title
+	local opacity = math.min(1, self.counter/self.fadeInLength)*255
+	love.graphics.setColor(255, 255, 255, opacity)
+	love.graphics.draw(self.title, 144, self.title_y)
+
+	--draw selections
+	local choice_y_factor = (self.selection_max_y - self.selection_min_y)/(#self.choices - 1)
+	for i, choice in ipairs(self.choices) do
+		if i == self.selection then
+			love.graphics.setColor(255, 100, 100, self.selection_opacity)
+		else
+			love.graphics.setColor(255, 255, 255, self.selection_opacity)
+		end
+		local y = self.selection_min_y + ((i-1)*choice_y_factor)
+		love.graphics.printf(choice[1], 0, y, 800, "center")
+	end
+
+	if self.fadingOut then
+		love.graphics.setColor(0, 0, 0, self.fadeoutOpacity)
+		love.graphics.rectangle("fill", 0, 0, 800, 600)
+	end
+end
+
+return state
