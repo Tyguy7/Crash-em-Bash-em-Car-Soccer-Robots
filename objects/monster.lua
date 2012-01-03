4a7b9de8d89dae083492971648c31b95343c808e
Cameron Seebach <cameron.seebach@gmail.com>
2012-01-03 10:27:21 -0800

    fixed truck size

--- objects/monster.lua
+++ objects/monster.lua
@@ -4,6 +4,7 @@ local Car = require "objects.car"
 
 local Monster = class("Monster", Car)
 Monster.static.IMAGE = "truck.png"
+Monster.static.WIDTH = 60
 Monster.static.HEIGHT = 32
 Monster.static.MASS = 15
 Monster.static.FORWARD = 210
