3413bd8797055f1aefefbfb154619b642f7af32a
Cameron Seebach <cameron.seebach@gmail.com>
2012-01-07 21:36:12 -0800

    added application identity (for local storage)

--- main.lua
+++ main.lua
@@ -2,6 +2,12 @@ local state = nil
 
 function love.load()
 	love.graphics.setMode(800, 600, true)
+
+	love.filesystem.setIdentity("Crash 'Em Bash 'Em")
+
+	logging = love.filesystem.newFile("logfile.txt")
+	logging:open("a")
+
 	state = require "states.mainmenu"
 	while not state:load() do end
 end
